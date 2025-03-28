2701dddd4c071877d3af6a12c81f725769b067ad
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-28 17:05:23 -0300

    feat: initial commit

---
+++ src/main/java/com/JJC/Social_midia/SocialMidiaApplication.java
@@ -0,0 +1,13 @@
+package com.JJC.Social_midia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMidiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMidiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JJC/Social_midia/SocialMidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JJC.Social_midia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
