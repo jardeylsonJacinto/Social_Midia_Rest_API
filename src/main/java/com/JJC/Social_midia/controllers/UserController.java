aec824b8aecee8bf72fecea389e61ba713488599
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-30 16:04:17 -0300

    refactor: Enhancing POST Method to return correct HTTP Status Code and Location URI

--- src/main/java/com/JJC/Social_midia/controllers/UserController.java
+++ src/main/java/com/JJC/Social_midia/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.JJC.Social_midia.controllers;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.JJC.Social_midia.model.User;
 import com.JJC.Social_midia.model.dao.UserDaoService;
@@ -19,21 +22,26 @@ public class UserController {
     this.service = service;
   }
 
-  // GET /users
   @GetMapping("/users")
   public List<User> retrieveAllUsers() {
     return service.findAll();
   }
 
-  // GET /users
   @GetMapping("/users/{id}")
   public User retrieveUser(@PathVariable int id) {
     return service.findOne(id);
   }
 
-  // POST /users
   @PostMapping("/users")
-  public void createUser(@RequestBody User user) {
-    service.save(user);
+  public ResponseEntity<User> createUser(@RequestBody User user) {
+
+    User savedUser = service.save(user);
+
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(savedUser.getId())
+        .toUri();
+
+    return ResponseEntity.created(location).build();
   }
 }
