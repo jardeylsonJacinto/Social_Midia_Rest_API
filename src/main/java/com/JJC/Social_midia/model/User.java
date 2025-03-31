6a26890868c19377c97e542be8d137e313c1fd5a
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-31 14:29:44 -0300

    feat:  Implementing HATEOAS for REST API, Static Filtering for REST API, Dynamic Filtering for REST API

--- src/main/java/com/JJC/Social_midia/model/User.java
+++ src/main/java/com/JJC/Social_midia/model/User.java
@@ -2,6 +2,10 @@ package com.JJC.Social_midia.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-
   private Integer id;
+
+  @Size(min = 2, message = "Name should have atleast 2 characters")
+  @JsonProperty("user_name")
   private String name;
+
+  @Past(message = "Birth Date should be in the past")
+  @JsonProperty("birth_date")
   private LocalDate birthDate;
 
   @Override
