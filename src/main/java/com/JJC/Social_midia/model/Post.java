0ad728171e7eabb7cc994162d8006a009b1c3cea
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-31 15:30:36 -0300

    feat: Creating User and Post Entity with Many to One Relationship with User Entity using JPA and Hibernate

--- src/main/java/com/JJC/Social_midia/model/Post.java
+++ src/main/java/com/JJC/Social_midia/model/Post.java
@@ -2,6 +2,11 @@ package com.JJC.Social_midia.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,13 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Post {
-
+  @Id
+  @GeneratedValue
   private Integer id;
 
   @Size(min = 10)
   private String description;
 
+  @ManyToOne(fetch = FetchType.LAZY)
   @JsonIgnore
   private User user;
 
--- src/main/java/com/JJC/Social_midia/model/User.java
+++ src/main/java/com/JJC/Social_midia/model/User.java
@@ -1,9 +1,14 @@
 package com.JJC.Social_midia.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,16 +18,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "user_details")
 public class User {
+  @Id
+  @GeneratedValue
   private Integer id;
 
   @Size(min = 2, message = "Name should have atleast 2 characters")
-  @JsonProperty("user_name")
   private String name;
 
   @Past(message = "Birth Date should be in the past")
-  @JsonProperty("birth_date")
   private LocalDate birthDate;
+
+  @OneToMany(mappedBy = "user")
+  @JsonIgnore
+  private List<Post> posts;
+
+  public User(Integer id, String name, LocalDate birthDate) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.birthDate = birthDate;
+  }
 
   @Override
   public String toString() {
